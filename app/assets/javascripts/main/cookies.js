f233e463b59571ea9f46fa6acc46e33771a25758
Ignat123 <ignat-nee@tut.by>
2015-02-27 11:07:05 +0300

    Precompile assets

--- app/assets/javascripts/main/cookies.js
+++ app/assets/javascripts/main/cookies.js
@@ -1,10 +1,16 @@
 function saveCookie(name, value){
   var CookieDate = new Date;
   CookieDate.setFullYear(CookieDate.getFullYear( ) +10);
-  document.cookie = name + "=" + value + "; path=/; expires=" + CookieDate + ";";
+  document.cookie = name + "=" + value + "; path=/;" + getNextYear();
 }
 
 function saveCookieWithReload(name, value){
   saveCookie(name, value);
   location.reload();
+}
+
+function getNextYear() {
+  var expiration_date = new Date();
+  expiration_date.setFullYear(expiration_date.getFullYear() + 1);
+  return "; expires=" + expiration_date.toGMTString();
 }
