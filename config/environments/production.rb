1dc0c839d519169f6ed29c554db86066268fa0db
Ignat123 <ignat-nee@tut.by>
2016-01-16 18:09:11 +0300

    Styles Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,21 +22,22 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.serve_static_assets = true
-
+  config.assets.paths << Rails.root.join("app", "assets", "fonts","assets/themes/default/assetes/fonts", "themes/default/assets/fonts")
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.assets.debug = true
+  config.assets.compress = false
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
@@ -78,18 +79,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  #devise mailer
-  config.action_mailer.default_url_options = { :host => 'http://anotherbookmanager.herokuapp.com/' }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :enable_starttls_auto => true,
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "http://anotherbookmanager.herokuapp.com/",
-      :authentication => :login,
-      :user_name => "RorPublicator@gmail.com",
-      :password => "gmail123123"
-  }
 end
